4738e59d4d2ddc2e2b081dee0f25e41e24cdb837
Rameel Haroon <RameelHaroonOff@gmail.com>
2025-06-14 22:33:47 +0500

    Refactored book element and added delete functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const myLibrary = [new Book("The Silent Patient", "Alex", 259)];
+let myLibrary = [new Book("The Silent Patient", "Alex", 259)];
 const container = document.querySelector(".container");
 const openBtn = document.querySelector("#open-btn");
 const closeBtn = document.querySelector("#close-btn");
+const deleteBtn = document.querySelector("#delete-btn");
 const modal = document.querySelector("#modal");
 const form = document.getElementById("form");
 
@@ -24,7 +25,18 @@ closeBtn.addEventListener("click", () =>{
   modal.close();
 });
 
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete-btn")) {
+    let idToRemove = e.target.dataset.bookId;
+    myLibrary = myLibrary.filter(book => book.id !== idToRemove);
+    displayBooks();
+  }
+});
+
 function Book(name, author, pages) {
+  if(!new.target){
+    throw Error("Use new keyword");
+  }
   this.id = crypto.randomUUID();
   this.name = name;
   this.author = author;
@@ -44,20 +56,26 @@ function displayBooks(){
 
     myLibrary.forEach(book => {
         const newBookElement = document.createElement("div");
-        newBookElement.className = "book-item";
+        newBookElement.classList.add("book-item");
 
         let bookName = document.createElement("h4");
         bookName.textContent = book.name;
 
         let bookAuthor = document.createElement("p");
-        bookAuthor.textContent = book.author;
+        bookAuthor.innerHTML = `<b>Author:</b>${book.author}`;
 
         let bookPages = document.createElement("p");
-        bookPages.textContent = book.pages;
+        bookPages.innerHTML = `<b>No of pages:</b>${book.pages}`;
+        
+        let bookDeleteBtn = document.createElement("button");
+        bookDeleteBtn.textContent = "Delete"; 
+        bookDeleteBtn.classList.add("delete-btn");
+        bookDeleteBtn.dataset.bookId = book.id;
 
         newBookElement.appendChild(bookName);
         newBookElement.appendChild(bookAuthor);
         newBookElement.appendChild(bookPages);
+        newBookElement.appendChild(bookDeleteBtn);
 
         container.appendChild(newBookElement);
     });
